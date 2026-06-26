de7f9faa4dbfcd1982c5bb992c63ee6b89d581ac
lily kim <72905578+kr3011@users.noreply.github.com>
2026-06-26 21:16:35 +0900

    최신가이드

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+// discord.js v14+ 표준 예시 (최신 가이드 참고)
+const { Client, GatewayIntentBits, Events } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, /* ...필요 인텐트 */] });
 
-const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent
-    ]
-});
-
-client.once('ready', () => {
-    console.log(`${client.user.tag} 봇이 온라인입니다!`);
-});
-
-client.on('messageCreate', (message) => {
-    if (message.author.bot) return; // 봇이 쓴 채팅은 무시
-
-    if (message.content === '!인사') {
-        message.reply(`반가워요, ${message.author.username}님! 👋`);
-    }
-});
+client.once(Events.ClientReady, c => console.log(`Ready! ${c.user.tag}`));
+// ...코드 
+client.login('TOKEN');
